cfdfb75ef7c35cfd6e9ab389a2b2d5eefb74e987
Aleksandra <aleksandra@MacBook-Air-Aleksandra.local>
2023-01-31 21:35:16 +0300

    fix: fix container type

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import { store } from './store/store';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
