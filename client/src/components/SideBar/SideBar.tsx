68d3f82076a1a215a1d72b232a0f3f9b84fb6a7d
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-02-02 18:20:17 +0300

    feat: add profile-page

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../store/hooks';
 import ButtonBurger from '../ButtonBurger/ButtonBurger';
 import './sideBar.css';
 
 const SideBar = () => {
   const [activeSidebar, setActiveSideBar] = useState(false);
+  /* const { role, id } = useAppSelector((state) => state.userInfo.userInfo); */
+  const [role, id] = ['parent', 1];
   return (
     <>
       <ul className={activeSidebar ? 'sidebar__ul-none' : 'sidebar__ul'}>
@@ -12,6 +15,12 @@ const SideBar = () => {
           <li className="sidebar__li active">
             <div className="sidebar__li-icon icon-main"></div>
             <div className="sidebar__li-text">Главная</div>
+          </li>
+        </Link>
+        <Link to={`${role}/${id}`}>
+          <li className="sidebar__li">
+            <div className="sidebar__li-icon icon-profile"></div>
+            <div className="sidebar__li-text">Профиль</div>
           </li>
         </Link>
         <Link to="/diary">
