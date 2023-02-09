ad6df362eb28418e8bf328597eb480c815fb89bf
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-02-09 08:26:32 +0300

    feat: add schedule with server

--- client/src/pages/SchedulePage/SchedulePage.tsx
+++ client/src/pages/SchedulePage/SchedulePage.tsx
@@ -1,27 +1,31 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import ScheduleDay from '../../components/ScheduleDay/ScheduleDay';
-import { schedule } from '../DiaryPage/DiaryPage';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { getSchedule } from '../../thunks/schedule';
 import './schedule.css';
-
-const renderShedule = () => {
-  return (
-    <div className="schedule__list">
-      {Object.keys(schedule).map((day, index) => {
-        return (
-          <ScheduleDay lessons={schedule[day]} day={day} key={index} index={index}></ScheduleDay>
-        );
-      })}
-    </div>
-  );
-};
 
 const SchedulePage = () => {
   const { t } = useTranslation();
+  const schedule = useAppSelector((state) => state.schedule.schedule);
+  const idClass = useAppSelector((state) => state.classInfo.classInfo.id);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (idClass) {
+      dispatch(getSchedule(idClass));
+    }
+  }, []);
 
   return (
     <div>
       <h2>{t('schedule.scheduleTitle')}</h2>
-      {renderShedule()}
+      {schedule &&
+        Object.keys(schedule).map((day, index) => {
+          return (
+            <ScheduleDay lessons={schedule[day]} day={day} key={index} index={index}></ScheduleDay>
+          );
+        })}
     </div>
   );
 };
