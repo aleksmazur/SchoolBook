f7feb948498072a07773a57607e5a1acd30b4682
Aleksandra <aleksandra@MacBook-Air-Aleksandra.local>
2023-02-10 14:44:16 +0300

    feat: add news img from server

--- client/src/components/NewsItem/NewsItem.tsx
+++ client/src/components/NewsItem/NewsItem.tsx
@@ -1,16 +1,19 @@
+import { BASE_URL } from '../../constants/baseUrl';
 import { INewsItem } from '../../reducers/newsReducer';
 import './newsItem.css';
 
-const NewsItem = ({ title, content }: INewsItem) => {
+const NewsItem = ({ title, content, image }: INewsItem) => {
   return (
     <div className="news__item">
       <div className="news__desc">
         <h4 className="news__title">{title}</h4>
         <div className="news__text">{content}</div>
       </div>
-      {/* <div className="news__photo">
-        <img src={photo} className="news__photo-image" alt="Фото" />
-      </div> */}
+      {image && (
+        <div className="news__photo">
+          <img src={`${BASE_URL}/${image}`} className="news__photo-image" alt="Фото" />
+        </div>
+      )}
     </div>
   );
 };
--- client/src/reducers/newsReducer.tsx
+++ client/src/reducers/newsReducer.tsx
@@ -1,14 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { getNews } from '../thunks/news';
 
-export type INewsItem = { id: number | null; title: string | null; content: string | null };
+export type INewsItem = {
+  id: number | null;
+  title: string | null;
+  content: string | null;
+  image: string | null;
+};
 
 type INews = {
   news: INewsItem[];
   isLoader: boolean;
 };
 
-const initialState: INews = { news: [{ id: null, title: null, content: null }], isLoader: false };
+const initialState: INews = {
+  news: [{ id: null, title: null, content: null, image: null }],
+  isLoader: false,
+};
 
 const newsReducer = createSlice({
   name: 'news',
