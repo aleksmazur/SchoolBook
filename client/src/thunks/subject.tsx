8c9184b2fcfd82b9677eeadc3e11f319c210c126
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-02-16 18:57:49 +0300

    fix: fixed token getSubject

--- client/src/thunks/subject.tsx
+++ client/src/thunks/subject.tsx
@@ -1,10 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { BASE_URL } from '../constants/baseUrl';
+import { TOKEN } from '../constants/token';
 
 export const getSubject = createAsyncThunk('subject/get', async () => {
   try {
     const response = await fetch(`${BASE_URL}/subjects`, {
       method: 'GET',
+      headers: {
+        Authorization: `Bearer ${TOKEN()}`,
+      },
     });
     if (response.status === 404) {
       throw new Error('Journal not Found');
