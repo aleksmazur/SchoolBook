645d322f8009af4766409f7fb523d2f543dbf6e5
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-02-16 19:41:25 +0300

    feat: add filterByClass

--- client/src/components/HomeWorkInJournal/HomeWorkInJournal.tsx
+++ client/src/components/HomeWorkInJournal/HomeWorkInJournal.tsx
@@ -5,26 +5,28 @@ import { getSubject } from '../../thunks/subject';
 import HomeWorkItem from '../HomeWorkItem/HomeWorkItem';
 import Preloader from '../Preloader/Preloader';
 
-type IHomeWork = {
+export type IHomeWork = {
   nameLesson: string;
   quarter: number;
+  idClass: number | null;
 };
 
-const HomeWorkInJournal = ({ nameLesson, quarter }: IHomeWork) => {
+const HomeWorkInJournal = ({ nameLesson, quarter, idClass }: IHomeWork) => {
   const dispatch = useAppDispatch();
   const { subjects, isLoader } = useAppSelector((state) => state.subjects);
   const [filtersSubject, setFiltersSubject] = useState<ISubjects[]>([]);
 
   useEffect(() => {
     dispatch(getSubject());
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     const res = subjects
       .filter((item) => item.name === nameLesson)
-      .filter((item) => item.quarter.quarter === quarter);
+      .filter((item) => item.quarter.quarter === quarter)
+      .filter((item) => item.classId === idClass);
     setFiltersSubject(res);
-  }, [subjects, nameLesson, quarter]);
+  }, [subjects, nameLesson, quarter, idClass]);
 
   return (
     <div className="homeWorkList__table">
