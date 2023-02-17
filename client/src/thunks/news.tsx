7977a0dd67c0268b15dbef225e37aa8e28e0483b
Aleksandra <aleksandra@MacBook-Air-Aleksandra.local>
2023-02-17 14:17:26 +0300

    feat: add form for adding news

--- client/src/thunks/news.tsx
+++ client/src/thunks/news.tsx
@@ -20,3 +20,22 @@ export const getNews = createAsyncThunk('news/get', async () => {
     throw new Error((err as Error).message);
   }
 });
+
+export const postNews = createAsyncThunk('news/post', async (formData: FormData) => {
+  try {
+    const response = await fetch(`${BASE_URL}/news`, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${TOKEN()}`,
+      },
+    });
+    if (response.status === 404) {
+      throw new Error('News not Found');
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    throw new Error((err as Error).message);
+  }
+});
