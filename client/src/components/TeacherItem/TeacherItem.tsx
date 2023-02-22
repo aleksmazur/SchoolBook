ac69e05a0eee8c887cec1ee511b760b2fc9cf1c6
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-02-22 09:56:22 +0300

    feat: add phone teacher

--- client/src/components/TeacherItem/TeacherItem.tsx
+++ client/src/components/TeacherItem/TeacherItem.tsx
@@ -1,20 +1,30 @@
+import { useTranslation } from 'react-i18next';
 import { BASE_URL } from '../../constants/baseUrl';
 import { useAppSelector } from '../../store/hooks';
+import './teachetItem.css';
 
 const TeacherItem = () => {
   const { classTeacher } = useAppSelector((state) => state.classInfo.classInfo);
+  const { t } = useTranslation();
 
   return (
-    <div className="teacher__item_name">
-      <div className="profile__photo">
-        <img
-          src={`${BASE_URL}/${classTeacher?.profilePic}`}
-          className="profile__photo-image"
-          alt="Фото"
-        />
+    <>
+      <div className="teacher__item_name">
+        <div className="profile__photo">
+          <img
+            src={`${BASE_URL}/${classTeacher?.profilePic}`}
+            className="profile__photo-image"
+            alt="Фото"
+          />
+        </div>
+        {classTeacher?.firstName + ' ' + classTeacher?.lastName}
       </div>
-      {classTeacher?.firstName + ' ' + classTeacher?.lastName}
-    </div>
+      <div className="profile__phone">
+        <a href={`tel: ${classTeacher?.phone}`}>
+          {t('profile.phone')}: {classTeacher?.phone}
+        </a>
+      </div>
+    </>
   );
 };
 
