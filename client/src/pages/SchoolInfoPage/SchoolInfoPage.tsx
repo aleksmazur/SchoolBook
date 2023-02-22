0aeeb3f7422ea8f186dc20b15ddf5eabdc326efe
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-02-23 01:34:11 +0300

    feat: add translate info school

--- client/src/pages/SchoolInfoPage/SchoolInfoPage.tsx
+++ client/src/pages/SchoolInfoPage/SchoolInfoPage.tsx
@@ -2,10 +2,12 @@ import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import Map from './../../components/Map/Map';
 import { getSchoolInfo } from '../../thunks/school';
+import { useTranslation } from 'react-i18next';
 
 const SchoolInfoPage = () => {
   const schoolInfo = useAppSelector((state) => state.schoolInfo.schoolInfo);
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
 
   useEffect(() => {
     dispatch(getSchoolInfo());
@@ -13,11 +15,15 @@ const SchoolInfoPage = () => {
 
   return (
     <div>
-      <h2>О школе</h2>
+      <h2>{t('info.about')}</h2>
       <div className="school__info">
         <h2>{schoolInfo.name}</h2>
-        <div>Адрес: {schoolInfo.adress}</div>
-        <div>Телефон: {schoolInfo.phone}</div>
+        <div>
+          {t('info.adress')}: {schoolInfo.adress}
+        </div>
+        <div>
+          {t('info.phone')}: {schoolInfo.phone}
+        </div>
         <div>E-mail: {schoolInfo.mail}</div>
       </div>
       <Map />
